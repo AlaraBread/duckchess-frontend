1570f4dd6d9a3656678cec46cec0500959e276b1
julia <59781761+AlaraBread@users.noreply.github.com>
2026-02-21 20:25:06 -0500

    update api url again

--- app/game_provider.tsx
+++ app/game_provider.tsx
@@ -117,7 +117,7 @@ interface GameContext {
 	setShouldConnect: (shouldConnect: boolean) => void;
 }
 
-export const API_URL = "https://duckchess.dabed.online";
+export const API_URL = "https://api.alarabread.fun";
 
 export function GameProvider({ children }: { children: React.ReactNode }) {
 	const login = useQuery({
